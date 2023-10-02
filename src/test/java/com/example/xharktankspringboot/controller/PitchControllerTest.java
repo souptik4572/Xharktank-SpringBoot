3d29f5904446f46092ff1b112949651dbc3391d8
Muhammad Saqib Chaudhry <young_innovator@yahoo.com>
2023-10-02 22:54:03 +0200

    unit tests added

--- src/test/java/com/example/xharktankspringboot/controller/PitchControllerTest.java
+++ src/test/java/com/example/xharktankspringboot/controller/PitchControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.xharktankspringboot.controller;
 
+import com.example.xharktankspringboot.domain.RestResponse;
 import com.example.xharktankspringboot.entity.Pitch;
 import com.example.xharktankspringboot.service.PitchService;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,8 +13,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Collections;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,5 +57,34 @@ class PitchControllerTest {
         mockMvc.perform(get(ROOT_PATH))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnSuccessResponseWithPitch() {
+        //Given
+        when(pitchService.getPitch(any())).thenReturn(Optional.of(new Pitch()));
+
+        //When
+        RestResponse response = pitchController.getPitch(1L);
+
+        //Then
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getResponse()).isInstanceOf(Pitch.class);
+    }
+
+    @Test
+    public void shouldReturnNotFoundResponseWithoutPitch() {
+        //Given
+        when(pitchService.getPitch(any())).thenReturn(Optional.empty());
+
+        //When
+        RestResponse response = pitchController.getPitch(1L);
+
+        //Then
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(NOT_FOUND);
+        assertThat(response.getResponse()).isInstanceOf(String.class);
+        assertThat(response.getResponse()).isEqualTo("There is no pitch exists with provide id.");
     }
 }
